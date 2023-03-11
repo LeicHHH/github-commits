3ad155b53366cb54246c32baca26aa152428ef22
Sebastián Mahuzier <smahu@DESKTOP-HOME-S.localdomain>
2023-03-11 20:32:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'github-commit',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
