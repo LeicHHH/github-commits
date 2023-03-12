24da8fdd25cb9cf8512f00472e0c5b2e1edb2012
Sebastián Mahuzier <smahu@DESKTOP-HOME-S.localdomain>
2023-03-12 19:24:26 -0300

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # github-commits
+##Prerequisites
+Before proceeding with the installation, ensure that you have the following software installed on your system:
+
+Node.js
+npm (Node Package Manager)
+Git (version control system)
+If you do not have these installed, you can download them from the official websites:
+
+Node.js: https://nodejs.org/
+npm: https://www.npmjs.com/get-npm
+Git: https://git-scm.com/downloads
+Installation
+Open your terminal or command prompt and run the following command to install Ionic globally:
+
+```npm install -g @ionic/cli```
+
+To clone an existing Ionic project, navigate to the directory where you want to clone the project:
+
+```git clone https://github.com/LeicHHH/github-commits.git```
+
+```cd github-commits```
+
+Install the project dependencies:
+
+```npm install```
+
+To run the project in your browser, run the following command:
+
+```ionic serve```
--- src/components/CommitList.tsx
+++ src/components/CommitList.tsx
@@ -1,35 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { IonList, IonItem, IonLabel, IonLoading } from '@ionic/react';
+import { IonList, IonItem, IonLabel, IonSpinner } from '@ionic/react';
 
 export const CommitList: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [commits, setCommits] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [commits, setCommits] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('https://api.github.com/repos/<username>/<repo>/commits');
-      setCommits(response.data);
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://api.github.com/repos/LeicHHH/github-commits/commits');
+                setCommits(response.data);
+                setLoading(false);
+            }
+            catch (e) {
+                console.log(e)
+            }
+        };
+        fetchData()
+            ;
+    }, []);
 
-  if (loading) {
-    return <IonLoading isOpen={true} message={'Loading commits...'} />;
-  }
+    if (loading) {
+        return <div className="ion-text-center" style={{ margin: '35vh 0 0 0', justifyContent: 'center'}} >
+            <IonSpinner name="lines" />
+            <p>Loading commits...</p>
+            </div>;
+    }
 
-  return (
-    <IonList>
-      {commits.map((commit: any) => (
-        <IonItem key={commit.sha}>
-          <IonLabel>
-            <h2>{commit.commit.message}</h2>
-            <p>{commit.commit.author.name}</p>
-            <p>{commit.commit.author.date}</p>
-          </IonLabel>
-        </IonItem>
-      ))}
-    </IonList>
-  );
+    return (
+        <IonList>
+            {commits.map((commit: any) => (
+                <IonItem key={commit.sha}>
+                    <IonLabel>
+                        <h2>{commit.commit.message}</h2>
+                        <p>{commit.commit.author.name}</p>
+                        <p>{commit.commit.author.date}</p>
+                    </IonLabel>
+                </IonItem>
+            ))}
+        </IonList>
+    );
 };
